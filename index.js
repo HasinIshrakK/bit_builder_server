e64ed37fa19bc54451cbf228849c5f1ee616f160
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-02-06 12:31:32 +0600

    feat: update server config / DB connection

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000;
+
+// middleWare
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Bit Builder is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Bit Builder listening on port ${port}`)
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.w0nmtjl.mongodb.net/?appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
+
+
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    
+  }
+}
+run().catch(console.dir);
