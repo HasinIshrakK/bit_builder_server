8543d0482a000ae911b2981c7eab65a93f23453d
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-02-06 15:41:31 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express")
 const cors = require("cors")
-const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -42,6 +42,13 @@ async function run() {
     app.get('/members', async (req, res)=> {
       const members = await membersCollection.find().toArray();
       res.send(members)
+    })
+
+    app.get('/members/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await membersCollection.findOne(query);
+      res.send(result)
     })
 
 
