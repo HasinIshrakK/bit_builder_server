9fcf10f85cdbfe9194d11e7b7c4bb295dc6dc294
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-02-06 15:46:36 +0600

    update

---
+++ member.js
@@ -0,0 +1,62 @@
+[
+  {
+    "teamName": "BitBuilder",
+    "name": "MD. HASIN ISHRAK KHAN",
+    "role": "Team Leader",
+    "email": "hasinishrakk@gmail.com",
+    "discord": "md.hasinishrakkhan",
+    "phone": "01518938360",
+    "facebook": "https://facebook.com/muhammadhasinishrak",
+    "image": "https://example.com/images/hasin.jpg"
+  },
+  {
+    "teamName": "BitBuilder",
+    "name": "Ahmed Abrar Zayad",
+    "role": "Member",
+    "email": "ahmedabrarzayad@gmail.com",
+    "discord": "ahmedabrar.",
+    "phone": "01768531042",
+    "facebook": "https://www.facebook.com/share/1Bb4zAdKaq/",
+    "image": "https://example.com/images/abrar.jpg"
+  },
+  {
+    "teamName": "BitBuilder",
+    "name": "Mahfuzur Rahman",
+    "role": "Member",
+    "email": "ripon301252@gmail.com",
+    "discord": "mahfuzurrahman_54734",
+    "phone": "01626607121",
+    "facebook": "https://www.facebook.com/profile.php?id=100089627922381",
+    "image": "https://example.com/images/mahfuzur.jpg"
+  },
+  {
+    "teamName": "BitBuilder",
+    "name": "Amina Akther Asha",
+    "role": "Member",
+    "email": "ashaamina91@gmail.com",
+    "discord": "amina.akther.asha",
+    "phone": "01621829947",
+    "facebook": "https://www.facebook.com/rongdhonu.satrong.169",
+    "image": "https://example.com/images/amina.jpg"
+  },
+  {
+    "teamName": "BitBuilder",
+    "name": "Faisal",
+    "role": "Member",
+    "email": "itsfaisalhossen@gmail.com",
+    "discord": "faisal_84856",
+    "phone": "01775352158",
+    "facebook": "https://www.facebook.com/itsfaisalhossen/",
+    "image": "https://example.com/images/faisal.jpg"
+  },
+  {
+    "teamName": "BitBuilder",
+    "name": "Tahmid Ahmed",
+    "role": "Member",
+    "email": "tahmid7282@gmail.com",
+    "discord": "tahmidman20",
+    "phone": "01937508287",
+    "facebook": "https://www.facebook.com/share/1DUQsxiiqJ",
+    "image": "https://example.com/images/tahmid.jpg"
+  }
+]
